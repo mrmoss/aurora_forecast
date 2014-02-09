65a8448783196c75d033e1a5a07f503cd48dd488
Mike Moss <mrmoss@alaska.edu>
2014-02-08 17:59:10 -0900

    Forecast Retriever retrieves...but doesn't do anything yet...

--- src/forecast_retriever.py
+++ src/forecast_retriever.py
@@ -1,24 +1,50 @@
 #!/usr/bin/python
 
+import signal
+import sys
+def signal_handler(signal,frame):
+	sys.exit(0)
+signal.signal(signal.SIGINT,signal_handler)
+
 import urllib2;
 
-now_cast_url="http://www.swpc.noaa.gov/ftpdir/lists/geomag/AK.txt";
-h1_cast_url="http://www.swpc.noaa.gov/ftpdir/lists/geomag/AK.txt";
-d3_cast_url="http://www.swpc.noaa.gov/ftpdir/forecasts/geomag_forecast/0120geomag_forecast.txt";
-d28_cast_url="http://www.swpc.noaa.gov/ftpdir/weekly/27DO.txt";
+forecast_url=[];
+forecast_url.append("http://www.swpc.noaa.gov/ftpdir/lists/geomag/AK.txt");
+forecast_url.append("http://www.swpc.noaa.gov/ftpdir/lists/geomag/AK.txt");
+forecast_url.append("http://www.swpc.noaa.gov/ftpdir/forecasts/geomag_forecast/0120geomag_forecast.txt");
+forecast_url.append("http://www.swpc.noaa.gov/ftpdir/weekly/27DO.txt");
 
-now_cast_request=urllib2.urlopen(now_cast_url);
-h1_cast_request=urllib2.urlopen(h1_cast_url);
-d3_cast_request=urllib2.urlopen(d3_cast_url);
-d28_cast_request=urllib2.urlopen(d28_cast_url);
+forecast_data=["","","",""];
 
-now_cast_data=now_cast_request.read();
-h1_cast_data=h1_cast_request.read();
-d3_cast_data=d3_cast_request.read();
-d28_cast_data=d28_cast_request.read();
 
-print(now_cast_data);
-print(h1_cast_data);
-print(d3_cast_data);
-print(d28_cast_data);
 
+
+
+
+
+
+
+
+
+
+import threading;
+
+def get_url(forecast):
+	while True:
+		try:
+			request=urllib2.urlopen(forecast_url[forecast]);
+			forecast_data[forecast]=request.read();
+			result[forecast]=True;
+			print "worked";
+		except:
+			print "error";
+
+for ii in range(0,4):
+	t = threading.Thread(target=get_url,args=[ii])
+	t.daemon = True
+	t.start()
+
+import time;
+
+while True:
+	time.sleep(0);
