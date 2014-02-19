53232e77918b86346ed36975f29deb2e16880efc
paul <beriukay+github@gmail.com>
2014-02-18 17:52:41 -0900

    initial push of 28-day forecaster with its own main

---
+++ src/forecast_parser_28_day.py
@@ -0,0 +1,33 @@
+from time import strptime
+
+def date_to_json(year, month, day):
+    formatted =  "'year' : " + str(year) 
+    formatted += ", 'month' : " + str(month)
+    formatted += ", 'day' : " + str(day)
+    return formatted
+
+def time_to_json(hour, minute):
+    formatted =  ", 'hour' : " + str(hour)
+    formatted += ", 'minute' : " + str(minute)
+    return formatted
+
+def parse28(text):
+    document = text.split("\n")
+    for line in document:
+	if line[0:4].isdigit():
+	    year = line[0:4]
+	    month = strptime(line[5:8], '%b').tm_mon
+	    day = line[9:11]
+	    kp_value = line[-1]
+	    json = "{ 'Time_predicted' : { " + date_to_json(year, month, day)
+	    json += time_to_json(-1, -1) + "} , 'Kp' : " + str(kp_value) + "}"
+
+	    print json
+	    
+
+def main():
+    parse28("2014 Feb 11    150		5	3\n2014 Feb 12	    111		5	2")
+
+
+if __name__ == "__main__":
+    main()
