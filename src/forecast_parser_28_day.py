790a205143c1ba3901d4073f63e432d73030c9b8
pcgentemann <beriukay+github@gmail.com>
2014-02-18 19:27:45 -0900

    forgot a comma

--- src/forecast_parser_28_day.py
+++ src/forecast_parser_28_day.py
@@ -1,5 +1,8 @@
 from time import strptime
 
+def get_current_date():
+
+
 def date_to_json(year, month, day):
     formatted =  '"year" : ' + str(year) 
     formatted += ', "month" : ' + str(month)
@@ -29,12 +32,10 @@ def parse28(input_text):
 	if line[0:4].isdigit():
 	    json = time_predicted(line[0:11])
 	    kp_value = line[-1]	# kp is all we need, and it is at the end of line.
-
-	    # Create output text
-	    json += ' "forecast" : "28day", "kp" : ' + str(kp_value) + '}, '
+	    json += ', "forecast" : "28day", "kp" : ' + str(kp_value) + '}, '
 
 	    output_text += json
-    # Remove the off-by-1 comma from the loop
+    # Remove the off-by-1 comma made by the loop.
     output_text = output_text[:-2]
     return output_text + ' ]'
 	    
