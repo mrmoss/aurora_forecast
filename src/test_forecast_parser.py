3bb26dabd020e03af6d3e001e8a19ad0a55de13f
rpkolesnik <rpkolesnik@alaska.edu>
2014-02-21 20:22:29 -0900

    Added tests for values, NOTE: MIGHT NEED TO INSTALL voluptuous module for python

--- src/test_forecast_parser.py
+++ src/test_forecast_parser.py
@@ -2,15 +2,15 @@
 
 # Forecast Parser Test
 #	Created By:	Ruslan Kolesnik, Caleb Hellickson, Ignacio Saez Lahildalga, Mike Moss
-#	Modified On:	02/16/2014
+#	Modified On:	02/21/2014
 
-import unittest
 import json
 import datetime
 from forecast_parser import parse
+from voluptuous import Schema, Required, All, Length, Range, MultipleInvalid, Invalid
 
 
-
+	
 
 class Test:
 	# Keeps track of number of failed tests
@@ -31,14 +31,14 @@ class Test:
 		self.json_string = json_string;
 
 	def print_error (self, error_msg):
-		print 65 * '-'
-		print error_msg;
+		print 60 * "-";
+		print "\033[1;31m" + error_msg + "\033[1;m";
 		Test.failed_tests += 1;
 
 	# Test if square brackets are used correctly in json string
 	def test_brackets (self):
 		if self.json_string[0] != '[' or self.json_string[-1] != ']':
-			print_error ("FAILED (First and last characters of the json string must be '[' and ']' respectively");
+			print_error ("\033[1;31mFAILED (First and last characters of the json string must be '[' and ']' respectively");
 
 		number_of_sbrackets = 0; 
 		for char in self.json_string:
@@ -46,16 +46,11 @@ class Test:
 				number_of_sbrackets += 1;
 		if number_of_sbrackets > 2:
 			self.print_error ("FAILED (Too many square brackets in json sring");
-			
 
-	
-	# Test if the second and second to last characters of json string are curly braces
-	#def test_curly_braces (self):
-	#	if self.json_string[1] != '{' or self.json_string[-2] != '}':
-	#		self.print_error ("FAILED (Second and second to last characters of json string must be '{' and '}' respectively");
 
+	# Test if json format matches the decided format
 	def test_json_format (self):
-		dic1 = {"time_stamp" : {"year" : 2000, "month" : 1, "day" : 1, "hour" : 1, "minute" : 1}, "time_predicted" : {"year" : 2000, "month" : 1, "day" : 1, "hour" : 1, "minute" : 1}, "forecast" : "now", "kp" : 7};
+		match = {"time_stamp" : {"year" : 2000, "month" : 1, "day" : 1, "hour" : 1, "minute" : 1}, "time_predicted" : {"year" : 2000, "month" : 1, "day" : 1, "hour" : 1, "minute" : 1}, "forecast" : "now", "kp" : 7};
 		within_dic = {"year" : 2000, "month" : 1, "day" : 1, "hour" : 1, "minute" : 1};
 		
 		counter = 0;
@@ -63,45 +58,90 @@ class Test:
 			counter += 1;
 			
 			# Check if dictionary has the correct keys
-			if dictionary.keys() != dic1.keys():
+			if dictionary.keys() != match.keys():
 				self.print_error ("FAILED (Object " + str(counter) + " in json string has incorrect or missing key)");
 				continue;
+			
+			if type(dictionary['time_stamp']) is not dict:
+				self.print_error ("FAILED (Object " + str(counter) + " in json string must contain time_stamp as an object)"); 
+			else:
+				# Check if dictionary keys within time_stamp dictionary match
+				if dictionary['time_stamp'].keys() != within_dic.keys():
+					self.print_error ("FAILED (Object " + str(counter) + " in json string has incorrect or missing key with time_stamp object)");
 
-			# Check if dictionary keys within time_stamp dictionary match
-			if dictionary['time_stamp'].keys() != within_dic.keys():
-				self.print_error ("FAILED (Object " + str(counter) + " in json string has incorrect or missing key with time_stamp object)");
+			if type(dictionary['time_predicted']) is not dict:
+				self.print_error ("FAILED (Object " + str(counter) + " in json string must contain time_predicted as an object)");
+			else:	
+				# Check if dictionary keys within time_predicted dictionary match
+				if dictionary['time_predicted'].keys() != within_dic.keys():
+					self.print_error ("FAILED (Object " + str(counter) + " in json string has incorrect or missing key within time predicted object)");
 
-			# Check if dictionary keys within time_predicted dictionary match
-			if dictionary['time_predicted'].keys() != within_dic.keys():
-				self.print_error ("FAILED (Object " + str(counter) + " in json string has incorrect or missing key within time predicted object)");
+	# Test json string using schema
+	def test_with_schema (self):
+		time = {
+			Required("year") : All(int, Range(min=1970, max=2014)),
+			Required("month") : All(int, Range(min=1, max=12)),
+			Required("day") : All(int, Range(min=1, max=31)),
+			Required("hour") : All(int, Range(min=-1, max=24)),
+			Required("minute") : All(int, Range(min=-1, max=60))
+		};
+	
+		forecast = All(unicode, Length(min=3, max=5));
 
+		kp = All(int, Range(min=0, max=9));
+		
+		schema = Schema({
+			Required("time_stamp") : time,
+			Required("time_predicted") : time,
+			Required("forecast") : forecast,
+			Required("kp") : kp
+		});
+
+		counter = 0;
+		for dictionary in self.json_object:
+			counter += 1;
+			try:	
+				schema(dictionary);
+			except MultipleInvalid as e:
+				exc = e;
+				self.print_error("FAILED (Object " + str(counter) + " in json string " + str(exc) + ")");
 
 			
 
-
-def main():
-	json_string = parse("2014 Feb 11    150		5	3\n2014 Feb 12	    111		5	2", "28d");
+def test_parser (json_string):
 	print json_string;
+	if json_string == "Cannot determine which prediction page":
+		print 60 * "-";
+		print "\033[1;31mFAILED (Cannot determine which prediction page)\033[1;m"
+		print 60 * "-";
+		return;
+
 	
 	test = Test(json_string);
 	
-	# Invalid json string can't do other tests
+	# Invalid json string can't run other tests because other tests are dependent on this one
 	if(Test.failed_tests == 0):
 		test.test_brackets();
-		#test.test_curly_braces();
 		test.test_json_format();
-
+		test.test_with_schema();
+	
+	
 	if Test.failed_tests == 0:
-		print "----------------------------------------------------------------";
+		print 60 * "-";
 		print "\033[1;32mPASSED all tests\033[1;m"
 	else:
-		print "----------------------------------------------------------------";
+		print 60 * "-";
 		print "\033[1;31mFAILED", Test.failed_tests, "tests\033[1;m"
 
 
-	print "----------------------------------------------------------------";	
-
+	print 60 * "-";
+	
+def main():
+	json_string = parse("2014 Feb 11    150		5	3\n2014 Feb 12	    111		5	2", "28d");
+	test_parser (json_string);
 	
 
-main();
+	
+if __name__ == "__main__":
+	main();
 	
